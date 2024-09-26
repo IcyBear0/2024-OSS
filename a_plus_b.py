fc97756be3acda6973293ce62896fd02b5d711e0
IcyBear0 <ddd982046@gmail.com>
2024-09-26 09:48:00 +0900

    프로그램 종료 알림 추가

--- a_plus_b.py
+++ a_plus_b.py
@@ -3,6 +3,7 @@ while True:
         x = int(input('첫 번째 정수를 입력하세요: '))
         y = int(input('두 번째 정수를 입력하세요: '))
         print(f'{x} + {y} = {x + y}')
+        print('프로그램을 종료합니다')
         break
     except ValueError:
         print("정수가 아닙니다. 정수를 넣어주세요!")
